6ef2fd3a2b1b31fa7df6f91c7971aa948065716a
murilomancinir <murilomancinicontato@gmail.com>
2026-05-16 17:00:18 -0300

    Delete projetofrete/projetofrete directory

--- projetofrete/projetofrete/config.py
+++
@@ -1,7 +0,0 @@
-DB_HOST = "localhost"
-DB_NAME = "fretes"
-DB_USER = "fretes"
-DB_PASS = "1234"
-GOOGLE_API_KEY = "AIzaSyA1VH8getQ9WABo7xCwL-2PCOyKbivJxs8"
-SECRET_KEY = "logistica2025_frete_seguro"
-
