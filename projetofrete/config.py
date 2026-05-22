11e59afc6df70ef8118925dc14845c00fdd5bc53
murilomancinir <murilomancinicontato@gmail.com>
2026-05-22 09:29:29 -0300

    Update config.py

--- projetofrete/config.py
+++ projetofrete/config.py
@@ -1,7 +1,7 @@
-DB_HOST = "localhost"
-DB_NAME = "fretes"
-DB_USER = "fretes"
-DB_PASS = "1234"
+DB_HOST = "dpg-d883jkog4nts73eoo1vg-a"
+DB_NAME = "logistica_t250"
+DB_USER = "logistica_t250_user"
+DB_PASS = "kIL51F06eKDQ7iI1L03oEt7wPifHX9Qh"
 GOOGLE_API_KEY = "AIzaSyDG8oIrpPO45gkrikn4p2hHoGZct2KmaXs"
 SECRET_KEY = "logistica2025_frete_seguro"
 
